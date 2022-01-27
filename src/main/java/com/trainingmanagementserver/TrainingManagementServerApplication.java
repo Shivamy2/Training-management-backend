161da8b898d1eec66e2807d191c0e045cbc05be6
shivamy2 <sy7052789@gmail.com>
2022-01-27 15:35:38 +0530

    changed database settings

--- src/main/java/com/trainingmanagementserver/TrainingManagementServerApplication.java
+++ src/main/java/com/trainingmanagementserver/TrainingManagementServerApplication.java
@@ -49,51 +49,59 @@ public class TrainingManagementServerApplication {
 		};
 	}
 
-//	@Bean
-//	public CommandLineRunner run(UserCredentialsService userCredentialsService, UserDetailService userDetailService, TraineesTrainerRepository traineesTrainerRepository) {
-//		return args -> {
-//			userCredentialsService.deleteUser(1);
-//			userCredentialsService.roleSave(new Role("ROLE_TRAINER"));
-//			userCredentialsService.roleSave(new Role("ROLE_TRAINEE"));
-//			userCredentialsService.userCredentialsSave(new UserCredentialsEntity
-//					("anonymous","secret", "anonymous@gmail.com", new ArrayList<>())
-//			);
-//			userCredentialsService.userCredentialsSave(new UserCredentialsEntity
-//					("testuser2","secret", "testuser2@gmail.com", new ArrayList<>())
-//			);
-//			userCredentialsService.userCredentialsSave(new UserCredentialsEntity
-//					("testuser3","secret", "testuser3@gmail.com", new ArrayList<>())
-//			);
-//			userCredentialsService.userCredentialsSave(new UserCredentialsEntity
-//					("testuser4","secret", "testuser4@gmail.com", new ArrayList<>())
-//			);
-//			userCredentialsService.userCredentialsSave(new UserCredentialsEntity
-//					("testuser5","secret", "testuser5@gmail.com", new ArrayList<>())
-//			);
-//			userCredentialsService.userCredentialsSave(new UserCredentialsEntity
-//					("testuser6","secret", "testuser6@gmail.com", new ArrayList<>())
-//			);
-//			userCredentialsService.userCredentialsSave(new UserCredentialsEntity
-//					("testuser7","secret", "testuser7@gmail.com", new ArrayList<>())
-//			);
-//			userCredentialsService.userCredentialsSave(new UserCredentialsEntity
-//					("testuser8","secret", "testuser8@gmail.com", new ArrayList<>())
-//			);
-//
-//			userCredentialsService.addRoleToUser("ROLE_TRAINER", "anonymous");
-//			userCredentialsService.addRoleToUser("ROLE_TRAINEE", "testuser8");
-//			userCredentialsService.addRoleToUser("ROLE_TRAINEE", "testuser2");
-//			userCredentialsService.addRoleToUser("ROLE_TRAINEE", "testuser3");
-//			userCredentialsService.addRoleToUser("ROLE_TRAINER", "testuser4");
-//			userCredentialsService.addRoleToUser("ROLE_TRAINEE", "testuser5");
-//			userCredentialsService.addRoleToUser("ROLE_TRAINER", "testuser6");
-//			userDetailService.save(new UserDetailsEntity(3, "Shivam", "Yadav", "02", "08", "2000", "H-292 Hindalco Colony", "Renukoot", "Sonbhadra", 231217, "7007723257", "MALE"));
-//			userDetailService.save(new UserDetailsEntity(4, "Test", "User 1", "23", "12", "2001", "HH-4 Hindalco Colony", "Renukoot", "Sonbhadra", 231217, "889723257", "FEMALE"));
-//			userDetailService.save(new UserDetailsEntity(5, "Test", "User 2", "21", "11", "2002", "L-232 Hindalco Colony", "Renukoot", "Sonbhadra", 231217, "6547723257", "FEMALE"));
-//			traineesTrainerRepository.save(new TraineesTrainer(10, 3));
-//			traineesTrainerRepository.save(new TraineesTrainer(4, 3));
-//			traineesTrainerRepository.save(new TraineesTrainer(5, 6));
-//			traineesTrainerRepository.save(new TraineesTrainer(7, 8));
-//		};
-//	}
+	// @Bean
+	// public CommandLineRunner run(UserCredentialsService userCredentialsService,
+	// UserDetailService userDetailService,
+	// TraineesTrainerRepository traineesTrainerRepository) {
+	// return args -> {
+	// userCredentialsService.deleteUser(1);
+	// userCredentialsService.roleSave(new Role("ROLE_TRAINER"));
+	// userCredentialsService.roleSave(new Role("ROLE_TRAINEE"));
+	// userCredentialsService.userCredentialsSave(new UserCredentialsEntity
+	// ("anonymous","secret", "anonymous@gmail.com", new ArrayList<>())
+	// );
+	// userCredentialsService.userCredentialsSave(new UserCredentialsEntity
+	// ("testuser2","secret", "testuser2@gmail.com", new ArrayList<>())
+	// );
+	// userCredentialsService.userCredentialsSave(new UserCredentialsEntity
+	// ("testuser3","secret", "testuser3@gmail.com", new ArrayList<>())
+	// );
+	// userCredentialsService.userCredentialsSave(new UserCredentialsEntity
+	// ("testuser4","secret", "testuser4@gmail.com", new ArrayList<>())
+	// );
+	// userCredentialsService.userCredentialsSave(new UserCredentialsEntity
+	// ("testuser5","secret", "testuser5@gmail.com", new ArrayList<>())
+	// );
+	// userCredentialsService.userCredentialsSave(new UserCredentialsEntity
+	// ("testuser6","secret", "testuser6@gmail.com", new ArrayList<>())
+	// );
+	// userCredentialsService.userCredentialsSave(new UserCredentialsEntity
+	// ("testuser7","secret", "testuser7@gmail.com", new ArrayList<>())
+	// );
+	// userCredentialsService.userCredentialsSave(new UserCredentialsEntity
+	// ("testuser8","secret", "testuser8@gmail.com", new ArrayList<>())
+	// );
+	//
+	// userCredentialsService.addRoleToUser("ROLE_TRAINER", "anonymous");
+	// userCredentialsService.addRoleToUser("ROLE_TRAINEE", "testuser8");
+	// userCredentialsService.addRoleToUser("ROLE_TRAINEE", "testuser2");
+	// userCredentialsService.addRoleToUser("ROLE_TRAINEE", "testuser3");
+	// userCredentialsService.addRoleToUser("ROLE_TRAINER", "testuser4");
+	// userCredentialsService.addRoleToUser("ROLE_TRAINEE", "testuser5");
+	// userCredentialsService.addRoleToUser("ROLE_TRAINER", "testuser6");
+	// userDetailService.save(new UserDetailsEntity(3, "Shivam", "Yadav", "02",
+	// "08", "2000", "H-292 Hindalco Colony", "Renukoot", "Sonbhadra", 231217,
+	// "7007723257", "MALE"));
+	// userDetailService.save(new UserDetailsEntity(4, "Test", "User 1", "23", "12",
+	// "2001", "HH-4 Hindalco Colony", "Renukoot", "Sonbhadra", 231217, "889723257",
+	// "FEMALE"));
+	// userDetailService.save(new UserDetailsEntity(5, "Test", "User 2", "21", "11",
+	// "2002", "L-232 Hindalco Colony", "Renukoot", "Sonbhadra", 231217,
+	// "6547723257", "FEMALE"));
+	// traineesTrainerRepository.save(new TraineesTrainer(10, 3));
+	// traineesTrainerRepository.save(new TraineesTrainer(4, 3));
+	// traineesTrainerRepository.save(new TraineesTrainer(5, 6));
+	// traineesTrainerRepository.save(new TraineesTrainer(7, 8));
+	// };
+	// }
 }
